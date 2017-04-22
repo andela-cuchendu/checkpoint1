1576954606d0ec5dd5e8d25ff53c16fda0a4c51b
andela_cuchendu <uchendu.chibueze@andela.com>
2017-04-22 11:10:47 +0100

    Clean up eslint for NewsActionTypes

--- src/constants/NewsActionTypes.js
+++ src/constants/NewsActionTypes.js
@@ -2,8 +2,8 @@ import keyMirror from 'keymirror';
 
 // Define action constants
 const NewsActionTypes = keyMirror({
-  GET_SOURCES: null,   // get news headlines based on sources
-  GET_NEWS: null,  // get news
+  GET_SOURCES: null,
+  GET_NEWS: null,
 });
 
 export default NewsActionTypes;
