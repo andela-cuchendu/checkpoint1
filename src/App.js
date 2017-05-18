539a75628dba171925060169bd57214bd2c901d8
andela_cuchendu <uchendu.chibueze@andela.com>
2017-05-18 18:44:10 +0100

    Travis test3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import Header from './components/header';
-import Logout from './components/Logout';
+import Logout from './components/logout';
 import NewsSources from './components/newssources';
 import News from './components/news';
 import Login from './components/login';
